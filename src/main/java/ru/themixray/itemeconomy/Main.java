c5403b8ca08fb8d96575a37d84f3c133480ec7f6
MeexReay <meexreay@gmail.com>
2024-12-29 15:01:01 +0300

    java project

---
+++ src/main/java/ru/themixray/itemeconomy/Main.java
@@ -0,0 +1,16 @@
+package ru.themixray.itemeconomy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
